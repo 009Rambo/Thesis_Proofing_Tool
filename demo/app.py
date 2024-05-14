b9de98c20195642df7fdd2e329afa641b64b23ab
ramesh <ramesh.pandey@tuni.fi>
2024-05-14 09:32:28 +0300

    some chnages to the font identifier Co-authored-by: timosiu <timo.siuruainen@tuni.fi>

--- demo/app.py
+++ demo/app.py
@@ -26,33 +26,16 @@ def upload_file():
         file_path = os.path.join(upload_folder, file.filename)
         file.save(file_path)
 
-<<<<<<< HEAD
         # Extract text content and analyze PDF
         text_content, text_blocks, pages = process_pdf(file_path)
-=======
-        # Extract text content from the PDF file
-        text = extract_text_from_pdf(file_path)
-
-        pages = count_pages(file_path)
-
-        stated_number_of_pages = compare_pages(text, pages)
-
->>>>>>> dd9c085c1d72c6fca7c3df7a17d8bf65269a8844
 
         return jsonify({
             'message': 'File uploaded successfully',
             'file_name': file.filename,
             'file_path': file_path,
-<<<<<<< HEAD
             'text_content': text_content,
             'text_blocks': text_blocks,
             'pages_amount': pages
-=======
-            'text_content': text,
-            'pages_amount' : pages,
-            'stated_equals_actual' : stated_number_of_pages
-
->>>>>>> dd9c085c1d72c6fca7c3df7a17d8bf65269a8844
         }), 200
 
     except Exception as e:
@@ -93,7 +76,6 @@ def analyze_pdf(file_path):
                             current_font_size = None
                             current_font_name = None
 
-<<<<<<< HEAD
                             for span in line["spans"]:
                                 text = span["text"].strip()
                                 font_size = span["size"]
@@ -137,9 +119,6 @@ def analyze_pdf(file_path):
 
     return text_blocks
 
-=======
-# Here we count the amount of pages in the file
->>>>>>> dd9c085c1d72c6fca7c3df7a17d8bf65269a8844
 def count_pages(file_path):
     pages = 0
     try:
@@ -159,7 +138,7 @@ def compare_pages(text, pages):
 
 # Extract the number of appendix pages and subtract from the total pages count
 # Next lines finds the amount of appendices stated on text and that amount is substracted from the total pages for English format
-# These lines should just pass on Finnish format since amount of pages includes appendices 
+# These lines should just pass on Finnish format since amount of pages includes appendices
     appendices_index = text.find("appendices")
     if appendices_index != -1:
         appendices_end_index = text.find("pages", appendices_index)
@@ -182,9 +161,6 @@ def compare_pages(text, pages):
         '"Opinnäytetyö 55 sivua, joista liitteitä 3 sivua", "Master’s thesis 65 pages, appendices 10 pages" or "Bachelor’s thesis 41 pages, appendices 10 pages".'\
         '\n Notice that on Finnish format the amount of pages includes appendices and on English format pages does not include appenidices.'
     return result
-
-
-
 
 
 if __name__ == '__main__':
