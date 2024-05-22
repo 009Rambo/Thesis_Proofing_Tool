7f907030cbbdc380bdf668b2b7de2687e20e663b
Timo Siuruainen <timo.siuruainen@tuni.fi>
2024-05-22 13:38:37 +0300

    Upgrades and small changes to url crawler

--- backend/app/crawler.py
+++ backend/app/crawler.py
@@ -1,21 +1,13 @@
 # URL crawler for references
 # (Currently) returns url and status code of website.
-# Also included: first version, which was a class. Current version is 10x faster.
-import requests
+
 import logging
 import aiohttp
 import asyncio
 import time
 
+#Crawl - pings url, returns status code & pinged url
 async def crawl(url, session):                
-        '''
-        try:
-            resp_code = requests.get(url, timeout=1).status_code
-        except Exception as e:
-            logging.error("Could not get url {}, error: {}".format(url, str(e)))
-            resp_code = 0
-
-        '''
         try:
             async with session.get(url) as resp:
                 resp_code = resp.status
@@ -26,58 +18,17 @@ async def crawl(url, session):
         newResult = [{'url': url}, {'resp': resp_code}]
         return newResult
         
-async def run(urls_to_visit):
-    start = time.time()
+async def run_crawler(urls_to_visit):
+    start = time.time() #timer
     results = []
-        
+    
     async with aiohttp.ClientSession() as session:
         try:
             newresult = await asyncio.gather(*(crawl(url, session) for url in urls_to_visit))
         except Exception as e:
             logging.error("Error in Crawler: " + str(e))
         results.append(newresult)
-        '''
-        while self.urls_to_visit:
-            url = self.urls_to_visit.pop(0)
-            logging.debug("Getting url: {}".format(url))
-            self.crawl(url)      
-        '''
+
     end = time.time()
-    print("Took {} seconds".format(end - start))
+    logging.debug("Took {} seconds to ping {} urls.".format(end - start, len(urls_to_visit)))
     return results
-
-'''
-class Crawler:
-
-    def __init__(self, urls=[]):
-        self.urls_to_visit = urls
-        self.results = [{"url": str, "code": str}]
-    
-    async def crawl(self, url, session):                
-        try:
-            async with session.get(url) as resp:
-                resp_code = resp.status
-        except Exception as e:
-            logging.error("Could not get url " + url + " , error: " + str(e))
-            resp_code = 0
-        
-        newResult = [{"url": url, "code": str(resp_code)}]
-        self.results.append(newResult)
-        
-    async def run(self):
-        start = time.time()
-        self.results.clear()
-        
-        async with aiohttp.ClientSession() as session:
-            try:
-                await asyncio.gather(*(self.crawl(url, session) for url in self.urls_to_visit))
-            except Exception as e:
-                logging.error("Error in Crawler: " + str(e))
-        end = time.time()
-        print("Took {} seconds".format(end - start))
-        return self.results
-'''
-# Test
-#if __name__ == "__main__":
-    #Crawler(urls=["https://www.google.com/", "https://www.facebook.com/"]).run()
-    #test = asyncio.run(Crawler(urls=["https://www.google.com/", "https://www.facebook.com/"]).run())
